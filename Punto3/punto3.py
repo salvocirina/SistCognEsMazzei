09354a6a1e337ceb05434a2f275464ab9d8ab09d
Salvo Cirinà <salvocia90@gmail.com>
2015-06-18 18:16:27 +0200

    python work

--- Punto3/punto3.py
+++ Punto3/punto3.py
@@ -1,13 +1,13 @@
 from nltk import load_parser
 parser = load_parser('G2.fcfg',trace=0)
-f = open('Sentences.txt')
+f = open('Sentences')
 lines = f.readlines()
 out = open('G2.txt','w')
 f.close()
 for line in lines:
     tokens = line.split()
-    trees = parser.nbest_parse(tokens)
+    trees = parser.parse(tokens)
     for tree in trees:
-        print tree.node['SEM']
-        out.write(str(tree.node['SEM'])+'\n')
+        print tree.label()['SEM']
+        out.write(str(tree.label()['SEM'])+'\n')
 out.close()
